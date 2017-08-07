ab2a232b03308d2e301e45790b933b3cd936682f
Shontae Hogan <shontaehogan@gmail.com>
2017-08-07 14:19:05 -0400

    debugged js to get 'todos' to appear in the list

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# toDoList
+Use Express to build a todo list application. In order to keep the list of todos, you will need to create some data structure and update it from Express, like this example:
+  const todos = [
+  "Wash the car"
+  ];
+
+  app.get("/", function (req, res) {
+    res.render('index', { todos: todos });
+    });
+
+    app.post("/", function (req, res) {
+      todos.push(req.body.todo);
+      res.redirect('/');
+      })
+You should be able to add new todos and mark todos as complete.
+
+Because your data is being stored in memory, it will disappear every time your app is restarted. It is easiest to seed some todos and completed todos at the start of your app so that you can see them after a restart.
--- index.js
+++ index.js
@@ -1,28 +1,28 @@
+// packages
+
 const express = require('express');
 const mustacheExpress = require('mustache-express');
-const bodyParser = require('bodyParser');
-// const fs = require('fs');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 const app = express();
 
+// boiler plate
 app.use(bodyParser());
-
 // tell express to use mustache and bodyparser
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 app.use(bodyParser.urlencoded({ extended: false}));
-// app.use(bodyParser.json());
-
-
+app.use(bodyParser.json());
 app.use(express.static('views'));
 
+// arrays
+let pendingTodos = [];
+let doneTodos = [];
+let last_id = 0;
 
-var pendingTodos = [];
 
-var doneTodos = [];
-
-var last_id = 0;
-
+// homepage
 app.get('/', function(req, res) {
   res.render(
     'form', {
@@ -37,20 +37,32 @@ app.get('/', function(req, res) {
 app.post('/', function(req, res) {
   last_id += 1;
 
-  // user has submitted a for to us.  we want to retrieve the text of the form so we can save it into a new todo objext
-  let todo_text = req.body.text;
+
+pendingTodos.push ({
+  id: last_id,
+  text: req.body.item
+
+});
+
+// refresh wiht new data
+res.redirect('/');
 
   // we need to create a new todo objext to store the text of the user's todo message
 
-  let todo_object = {id: last_id,text: todo_text};
+  // let todo_object = {id: last_id,text: todo_text};
 
 
   // add the todo object to  array of pending todo's
-  pendingTodos.push(todo_object);
+  // pendingTodos.push({
+  //   id: last_id,
+  //   text: req.body.item
+  // });
+
 
   // redirect the user back to homepage and force a refresh
-  res.redirect('/');
 });
+
+// submit page
 
 app.post('/:id', function(req, res) {
   // this is the ID for the todo that the user clicked "mark complete" on
@@ -58,24 +70,20 @@ app.post('/:id', function(req, res) {
 
   // find pending todo objects that match the supplied ID
   // then store them into an array for further processing
-  pending = pendingTodos.filter(function(obj) {
-    return obj.id == id;
+  pending = pendingTodos.filter(function(li) {
+    return li.id == id;
   });
-
   // remove each pending todo object that matches the id requested from the pending array and add it to the done array
-  pending.forEach(function(obj) {
+  pending.forEach(function(li) {
       // find index the targeted todo object is in the pending array
-      var index = pendingTodos.indexOf(obj);
+      let index = pendingTodos.indexOf(li);
       if (index != -1) {
-
 // remove targeted todo object from pending array
 pendingTodos.splice(index, 1);
 }
-
     // add our targeted todo object to our done array
-    doneTodos.push(obj);
+    doneTodos.push(li);
   });
-
 // redirect the user back to the home page so the page will be forced to refresh with the newly modified arrays
 res.redirect('/');
 });
